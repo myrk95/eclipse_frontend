f1876c845da2d7ed6e1346ca4a4c716bfe77e9ea
Eloy Dominguez <eloydominguez3@gmail.com>
2025-12-16 18:13:38 +0100

    Navbar y history

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/logo.png';  
 import Modal from './Modal'
@@ -7,6 +7,8 @@ import Modal from './Modal'
 const Navbar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isModalRegisterOpen, setIsModalRegisterOpen] = useState(false);
+  const [focus, setFocus ] = useState(1)
+   const location = useLocation();
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -15,21 +17,20 @@ const Navbar = () => {
           <img src={logo} alt="SkinScan AI Logo" className="brand-logo-img" />
           <span className="brand-name">ECLIPSE</span>
         </div>
-        
-        <ul className="navbar-menu">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">Inicio</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/scanner" className="nav-link active">Escáner</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/history" className="nav-link">Historial</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">Acerca de</Link>
-          </li>
-        </ul>
+      <ul className="navbar-menu">
+        <li className="nav-item">
+          <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Inicio</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/scanner" className={location.pathname === "/scanner" ? "nav-link active" : "nav-link"}>Escáner</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/history" className={location.pathname === "/history" ? "nav-link active" : "nav-link"}>Historial</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>Acerca de</Link>
+        </li>
+      </ul>
         
         <div className="navbar-actions">
           <button className="btn login-btn" onClick={() => setIsModalOpen(true)}>Iniciar Sesion</button>
